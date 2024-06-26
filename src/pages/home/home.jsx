b24a546681feca0aa31d808bc75292217b259996
Gourav2609 <herculeswarrior.in@gmail.com>
2024-06-26 15:36:43 +0530

    Initial

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,7 +1,19 @@
+import React from "react";
+import Cauldron from "../../assets/cauldron.png";
+
 export default function Home() {
     return (
-        <div>
-            <h1>Home</h1>
-        </div>
-    )
+        <>
+            <div className="bg-black w-full">
+                <div className="container mx-auto">
+                    <div className="flex justify-between items-center py-4">
+                        <div className="flex ">
+                            <div><img src={Cauldron} alt="Cauldron" className="w-10 h-10" /></div>
+                            <div><p className="text-white">Cauldron</p></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
