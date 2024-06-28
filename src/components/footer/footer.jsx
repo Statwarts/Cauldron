80c7e8fc0284c890d4788809c356c2866de7539c
Divyam Sharma <113417008+div02-afk@users.noreply.github.com>
2024-06-28 22:34:04 +0530

    added footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -10,13 +10,13 @@ export default function Footer() {
         <>
         <div className="bg-[rgb(11,11,14)] text-white">
             <div className="flex flex-wrap-reverse justify-between mb-20">
-                <div className="flex-col flex  gap-2 m-auto lg:ml-32 lg:justify-normal justify-center lg:pt-0 pt-20 sm:pl-10 lg:pl-0">
-                    <img src = {Cauldron} alt = "Cauldron" width={"85px"} className="m-auto md:m-0" />
+                <div className="flex-col flex gap-2 m-auto lg:ml-32 lg:justify-normal justify-center md:pt-0 pt-20 pl-2 md:pl-10 lg:pl-0">
+                    <img src = {Cauldron} alt = "Cauldron" className="m-auto sm:m-0 w-[70px] lg:w-[85px]" />
                     <div className="text-transparent font-semibold bg-clip-text bg-gradient-to-b from-white to-[rgb(207,231,255)]">
-                    <p className=" md:text-6xl text-4xl text-center md:text-balance font-[UncutSans]">Cauldron</p>
+                    <p className=" md:text-6xl text-5xl text-center md:text-balance font-[UncutSans]">Cauldron</p>
                     </div>
-                    <p className="text-[rgb(227,231,234,0.7)] text-sm mb-10 text-center md:text-balance">© 2024 Cauldron All rights reserved.</p>
-                    <div className="flex gap-5 items-center m-auto lg:m-0">
+                    <p className="text-[rgb(227,231,234,0.7)] md:text-sm mb-10 text-center md:text-balance text-xs">© 2024 Cauldron All rights reserved.</p>
+                    <div className="flex gap-5 items-center m-auto md:m-0">
                         <Link to = "">
                         <img src = {Instagram} alt = "Instagram" width={"32px"} className=" drop-shadow-socialMedia" />
                         </Link>
@@ -29,7 +29,7 @@ export default function Footer() {
                     </div>
                 </div>
                 <div className=" mt-16 m-auto lg:mr-32">
-                    <p className="mb-10 text-center text-[rgb(188,207,226)] text-xl lg:text-2xl pl-10 pr-10 max-w-md">Sign up for the <span>waitlist</span>  and be the first to try Cauldron!</p>
+                    <p className="mb-10 text-center text-[rgb(188,207,226)] text-xl lg:text-2xl pl-10 pr-10 max-w-md">Sign up for the <span className="">waitlist</span>  and be the first to try Cauldron!</p>
                     <div className="flex justify-center"><Input /></div>
                 </div>
             </div>
