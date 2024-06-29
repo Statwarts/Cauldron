cad792c5d94bb8ed414a79e684acf5e2ffe1c0b0
Divyam Sharma <113417008+div02-afk@users.noreply.github.com>
2024-06-29 07:24:30 +0530

    minor

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -7,51 +7,78 @@ import Github from "../../assets/github.svg";
 import Linkedin from "../../assets/linkedin.svg";
 import { motion } from "framer-motion";
 export default function Footer() {
-    return (
-        <>
-        <div className="bg-[rgb(11,11,14)] text-white">
-            <div className="flex flex-wrap-reverse justify-between mb-20">
-                <div className="flex-col flex gap-2 m-auto lg:ml-32 lg:justify-normal justify-center md:pt-0 pt-20 pl-2 md:pl-10 lg:pl-0">
-                    <img src = {Cauldron} alt = "Cauldron" className="m-auto sm:m-0 w-[70px] lg:w-[85px]" />
-                    <div className="text-transparent font-semibold bg-clip-text bg-gradient-to-b from-white to-[rgb(207,231,255)]">
-                    <p className=" md:text-6xl text-5xl text-center md:text-balance font-[UncutSans]">Cauldron</p>
-                    </div>
-                    <p className="text-[rgb(227,231,234,0.7)] md:text-sm mb-10 text-center md:text-balance text-xs">© 2024 Cauldron All rights reserved.</p>
-                    <div className="flex gap-5 items-center m-auto md:m-0">
-                        <Link to = "">
-                        <img src = {Instagram} alt = "Instagram" width={"32px"} className=" drop-shadow-socialMedia" />
-                        </Link>
-                        <Link to = "https://github.com/Statwarts">
-                        <img src = {Github} alt = "Github" width={"32px"} className=" drop-shadow-socialMedia"/>
-                        </Link>
-                        <Link to = "">
-                        <img src = {Linkedin} alt = "Linkedin" width={"32px"} className=" drop-shadow-socialMedia"/>
-                        </Link>
-                    </div>
-                </div>
-                <div className=" mt-16 m-auto lg:mr-32">
-                    <p className="mb-10 text-center text-[rgb(188,207,226)] text-xl lg:text-2xl pl-10 pr-10 max-w-md">Sign up for the <span className="git ">waitlist</span>  and be the first to try Cauldron!</p>
-                    <div className="flex justify-center"><Input /></div>
-                </div>
+  return (
+    <>
+      <div className="bg-[rgb(11,11,14)] text-white">
+        <div className="mb-20 flex flex-wrap-reverse justify-between">
+          <div className="m-auto flex flex-col justify-center gap-2 pl-2 pt-20 md:pl-10 md:pt-0 lg:ml-32 lg:justify-normal lg:pl-0">
+            <img
+              src={Cauldron}
+              alt="Cauldron"
+              className="m-auto w-[70px] sm:m-0 lg:w-[85px]"
+            />
+            <div className="bg-gradient-to-b from-white to-[rgb(207,231,255)] bg-clip-text font-semibold text-transparent">
+              <p className="text-center font-[UncutSans] text-5xl md:text-balance md:text-6xl">
+                Cauldron
+              </p>
             </div>
-            <div className="flex justify-center gap-10 pb-6 text-[rgb(227,231,234,0.7)] text-sm">
-                <motion.div
-                    whileHover = {{
-                        color: "#0071E3"
-                    }}
-                >
-
-                <Link to= "/">Terms of Service</Link>
-                </motion.div>
-                <motion.div
-                    whileHover = {{
-                        color: "#0071E3"
-                    }}
-                >
-                <Link to= "/">Privacy Policy</Link>
-                </motion.div>
+            <p className="mb-10 text-center text-xs text-[rgb(227,231,234,0.7)] md:text-balance md:text-sm">
+              © 2024 Cauldron All rights reserved.
+            </p>
+            <div className="m-auto flex items-center gap-5 md:m-0">
+              <Link to="">
+                <img
+                  src={Instagram}
+                  alt="Instagram"
+                  width={"32px"}
+                  className="drop-shadow-socialMedia"
+                />
+              </Link>
+              <Link to="https://github.com/Statwarts">
+                <img
+                  src={Github}
+                  alt="Github"
+                  width={"32px"}
+                  className="drop-shadow-socialMedia"
+                />
+              </Link>
+              <Link to="">
+                <img
+                  src={Linkedin}
+                  alt="Linkedin"
+                  width={"32px"}
+                  className="drop-shadow-socialMedia"
+                />
+              </Link>
             </div>
+          </div>
+          <div className="m-auto mt-16 lg:mr-32">
+            <p className="mb-10 max-w-md pl-10 pr-10 text-center text-xl text-[rgb(188,207,226)] lg:text-2xl">
+              Sign up for the <span className="git">waitlist</span> and be the
+              first to try Cauldron!
+            </p>
+            <div className="flex justify-center">
+              <Input />
+            </div>
+          </div>
         </div>
-        </>
-    );
+        <div className="flex justify-center gap-10 pb-6 text-sm text-[rgb(227,231,234,0.7)]">
+          <motion.div
+            whileHover={{
+              color: "#0071E3",
+            }}
+          >
+            <Link to="/">Terms of Service</Link>
+          </motion.div>
+          <motion.div
+            whileHover={{
+              color: "#0071E3",
+            }}
+          >
+            <Link to="/">Privacy Policy</Link>
+          </motion.div>
+        </div>
+      </div>
+    </>
+  );
 }
--- src/components/whyCauldron.jsx
+++ src/components/whyCauldron.jsx
@@ -1,99 +1,117 @@
 import whyIcon1 from "../assets/whyIcon1.svg";
 import whyIcon2 from "../assets/whyIcon2.svg";
 import whyIcon3 from "../assets/whyIcon3.svg";
-import React,{useState,useEffect} from "react";
-import {motion} from "framer-motion";
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
 export default function WhyCauldron() {
-
-    const [isHovering, setIsHovering] = useState([false,false,false]);
-    const numOfLines = (text) => {
-        if(text.length > 100) {
-            return " md:max-w-[420px]";
-        }
-        return " md:max-w-[260px]";
+  const [isHovering, setIsHovering] = useState([false, false, false]);
+  const numOfLines = (text) => {
+    if (text.length > 100) {
+      return " md:max-w-[420px]";
     }
-    const data = [
-        {text : "Faceless videos allow for a consistent visual style and branding that isn’t reliant on the creator’s appearance, which can be more scalable in the long term."},
-        {text : "Change voices and choose voices that best match their brand or the tone of the content."},
-        {text : "Significantly reduce expenses related to hiring videographers and editors."},
-        {text : "Customizable subtitles ensure that videos are accessible to a broader audience, including those who are deaf or hard of hearing, and can enhance viewer engagement."},
+    return " md:max-w-[260px]";
+  };
+  const data = [
+    {
+      text: "Faceless videos allow for a consistent visual style and branding that isn’t reliant on the creator’s appearance, which can be more scalable in the long term.",
+    },
+    {
+      text: "Change voices and choose voices that best match their brand or the tone of the content.",
+    },
+    {
+      text: "Significantly reduce expenses related to hiring videographers and editors.",
+    },
+    {
+      text: "Customizable subtitles ensure that videos are accessible to a broader audience, including those who are deaf or hard of hearing, and can enhance viewer engagement.",
+    },
+  ];
+  const batchProcessingData = [
+    {
+      imgSrc: whyIcon1,
+      text: "Streamline the video production process, and focus more on the content rather than the technical aspects of video creation.",
+    },
+    {
+      imgSrc: whyIcon2,
+      text: "Producing consistent quality and style across all videos, which helps in maintaining a cohesive brand image.",
+    },
+    {
+      imgSrc: whyIcon3,
+      text: "Easily scale the production, creating more content in less time and potentially increasing their reach and engagement.",
+    },
+  ];
 
-    ];
-    const batchProcessingData = [
-        {
-            imgSrc : whyIcon1,
-            text : "Streamline the video production process, and focus more on the content rather than the technical aspects of video creation."
-        },
-        {
-            imgSrc : whyIcon2,
-            text : "Producing consistent quality and style across all videos, which helps in maintaining a cohesive brand image.",
-        },
-        {
-            imgSrc : whyIcon3,
-            text : "Easily scale the production, creating more content in less time and potentially increasing their reach and engagement."
-        }
-    ]
-
-    return(
-        <div className="bg-[rgb(11,11,14)] text-white pl-6 pr-6" >
-            <div className="m-auto max-w-[700px] text-[rgb(193,201,207)] pb-40">
-                <h1 className="text-4xl font-semibold ">Reduced Production Costs</h1>
-                <p className="max-w-3xl mb-20">Efficient video production with faceless content ensures cost-effectiveness and scalability, driven by a prompt.</p>
-                <div className="flex justify-center sm:justify-between flex-wrap text-[rgb(184,193,199,0.8)]">
-                    {
-                        data.map(({text}) => (
-                        <div className={"text-pretty mb-4" + numOfLines(text)}>
-                            <div className="h-28 bg-[rgb(22,24,25)] rounded-2xl mb-1"></div>
-                            <p className="pl-2 ">{text}</p>
-                        </div>
-                        ))
-                        
-                    }
-                    
-                </div>
-
+  return (
+    <div className="bg-[rgb(11,11,14)] pl-6 pr-6 text-white">
+      <div className="m-auto max-w-[700px] pb-40 text-[rgb(193,201,207)]">
+        <h1 className="text-4xl font-semibold">Reduced Production Costs</h1>
+        <p className="mb-20 max-w-3xl">
+          Efficient video production with faceless content ensures
+          cost-effectiveness and scalability, driven by a prompt.
+        </p>
+        <div className="flex flex-wrap justify-center text-[rgb(184,193,199,0.8)] sm:justify-between">
+          {data.map(({ text }) => (
+            <div className={"mb-4 text-pretty" + numOfLines(text)}>
+              <div className="mb-1 h-28 rounded-2xl bg-[rgb(22,24,25)]"></div>
+              <p className="pl-2">{text}</p>
             </div>
-
-            <div className="m-auto max-w-[960px] text-[rgb(193,201,207)] pb-40 ">
-                <h1 className="text-4xl font-semibold pl-5 lg:pl-0 ">Batch Processing</h1>
-                <p className="max-w-3xl lg:max-w-[900px] mb-20 pl-5 lg:pl-0">The ability to create multiple videos in a short amount of time is particularly useful for creators who need to maintain a consistent posting schedule.</p>
-                <div className="flex flex-wrap justify-center">
-                    {batchProcessingData.map(({imgSrc, text},index) => (
-                        <React.Fragment key={index}>
-                        <div className="flex flex-col justify-center text-pretty items-start ">
-                            
-                            <motion.img 
-                            onHoverStart={() => {
-                                const newHovering = [...isHovering];
-                                newHovering[index] = true;
-                                setIsHovering(newHovering)
-                            }}
-                            onHoverEnd={() => {
-                                const newHovering = [...isHovering];
-                                newHovering[index] = false;
-                                setIsHovering(newHovering)}}
-                            src={imgSrc} alt="icon" className="w-[96px] m-auto mb-4 " 
-                             animate={isHovering[index]? {
-                                filter : 'drop-shadow(0px 5px 10px rgba(255,255,255,0.5))'
-                            } : {
-                                filter : 'drop-shadow(0px 0px 0px rgba(0, 0, 0, 0.5))'
-                            }  }
-                            transition={{duration:0.5}}
-                            exit={{ filter: '' }}
-                            />
-                            
-                            <div className="m-auto p-1 pb-4">
-                                <p className="max-w-[500px] md:max-w-[300px] text-[14px] sm:p-2 md:pl-6 text-[rgb(184,193,199,0.8)]">{text}</p>
-                            </div>
-                        </div>
-                        {index  < batchProcessingData.length - 1 && (
-                            <div className="lg:w-1 bg-[#3A3F45] md:h-48 lg:ml-1"></div>
-                        )}
-                        </React.Fragment>   
-                    ))}
-                </div>
-
-            </div>
+          ))}
         </div>
-    )
+      </div>
+
+      <div className="m-auto max-w-[960px] pb-40 text-[rgb(193,201,207)]">
+        <h1 className="pl-5 text-4xl font-semibold lg:pl-0">
+          Batch Processing
+        </h1>
+        <p className="mb-20 max-w-3xl pl-5 lg:max-w-[900px] lg:pl-0">
+          The ability to create multiple videos in a short amount of time is
+          particularly useful for creators who need to maintain a consistent
+          posting schedule.
+        </p>
+        <div className="flex flex-wrap justify-center">
+          {batchProcessingData.map(({ imgSrc, text }, index) => (
+            <React.Fragment key={index}>
+              <div className="flex flex-col items-start justify-center text-pretty">
+                <motion.img
+                  onHoverStart={() => {
+                    const newHovering = [...isHovering];
+                    newHovering[index] = true;
+                    setIsHovering(newHovering);
+                  }}
+                  onHoverEnd={() => {
+                    const newHovering = [...isHovering];
+                    newHovering[index] = false;
+                    setIsHovering(newHovering);
+                  }}
+                  src={imgSrc}
+                  alt="icon"
+                  className="m-auto mb-4 w-[96px]"
+                  animate={
+                    isHovering[index]
+                      ? {
+                          filter:
+                            "drop-shadow(0px 5px 10px rgba(255,255,255,0.5))",
+                        }
+                      : {
+                          filter: "drop-shadow(0px 0px 0px rgba(0, 0, 0, 0.5))",
+                        }
+                  }
+                  transition={{ duration: 0.5 }}
+                  exit={{ filter: "" }}
+                />
+
+                <div className="m-auto p-1 pb-4">
+                  <p className="max-w-[500px] text-[14px] text-[rgb(184,193,199,0.8)] sm:p-2 md:max-w-[300px] md:pl-6">
+                    {text}
+                  </p>
+                </div>
+              </div>
+              {index < batchProcessingData.length - 1 && (
+                <div className="bg-[#3A3F45] md:h-48 lg:ml-1 lg:w-1"></div>
+              )}
+            </React.Fragment>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
