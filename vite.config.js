6687d34b23d97e044aca3eaaefda39122aba96fc
Divyam Sharma <113417008+div02-afk@users.noreply.github.com>
2024-07-15 21:17:39 +0530

    removes logs in prod

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import removeConsole from 'vite-plugin-remove-console';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    removeConsole({ 
+      include: ['log', 'warn', 'error'],
+      exclude: ['info']
+    })
+  ]
+});
