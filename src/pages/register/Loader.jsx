65243affbade3d63ce51ffadcad043feb4920df7
Gourav2609 <herculeswarrior.in@gmail.com>
2024-07-15 17:26:55 +0530

    Register_loader

--- src/pages/register/Loader.jsx
+++ src/pages/register/Loader.jsx
@@ -1,31 +1,40 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
+
 const Loader = () => {
   const videoRef = useRef(null);
+  const [videoSrc, setVideoSrc] = useState("");
 
   useEffect(() => {
     if (videoRef.current) {
       videoRef.current.playbackRate = 1.5;
     }
+    const loadVideo = async () => {
+      const videoModule = await import("../src/assets/Videos/Loader.mp4");
+      setVideoSrc(videoModule.default);
+    };
+
+    loadVideo();
   }, []);
 
   const maskstyle = {
     maskImage: "radial-gradient(circle, white 50%, transparent 0%)",
     WebkitMaskImage: "radial-gradient(circle, white 50%, transparent 0%)",
   };
+
   return (
-    <>
-      <div className="">
+    <div className="">
+      {videoSrc && (
         <video
           ref={videoRef}
           style={maskstyle}
-          src="../src/assets/videos/Loader.mp4"
+          src={videoSrc}
           width={300}
           autoPlay
           loop
           draggable={false}
         />
-      </div>
-    </>
+      )}
+    </div>
   );
 };
 
