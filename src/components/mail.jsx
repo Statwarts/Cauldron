40d579ef031e6a81217afd54e0ae6397d8e0a903
Divyam Sharma <113417008+div02-afk@users.noreply.github.com>
2024-07-28 01:18:34 +0530

    mail fix and prompt updated

--- src/components/mail.jsx
+++ src/components/mail.jsx
@@ -1,101 +1,66 @@
 import React from "react";
 
 const Mail = (name) => {
-  return `<div className="wrap">
-    <img className="logo" src="https://github.com/Statwarts/Cauldron/blob/main/src/assets/svg/cauldron.svg" alt="Cauldron" />
-      <h1 className="header">Welcome to Cauldron!</h1>
-      <br />
-      <div className="content">
-        <p className="paragraph">Hello ${name},</p>
-        <p className="paragraph">
-          Thank you for your interest in being one of Cauldron's first users. We
-          truly appreciate your support!
-        </p>
-        <p className="paragraph">
-          We've been working diligently on 
-          <a className="link" href="https://cauldron.live">
-            cauldron.live
-          </a>
-          , since May 2024, and we're excited to announce that it will be
-          released in August. Stay tuned for an incredible experience!
-        </p>
-        <p className="paragraph">
-          To get the latest updates, follow us on Twitter: 
-          <a className="link" href="https://x.com/cauldron_live">
-             @cauldron_live
-          </a>
-        </p>
-        <p className="paragraph">
-          Right now it's just 3 people working on this idea we believe is quite
-          amazing. We have colleges, examinations, internships, and other
-          projects, but we will ship this very soon.
-        </p>
-        <p className="paragraph">
-          Stay tuned and thank you for joining the queue.
-        </p>
-        <br />
-        <p className="paragraph">Best regards,</p>
-        <p className="team">The Cauldron Team</p>
-      </div>
-      <br />
-      <div className="footer">
-        <p className="footer-text">&copy; 2024 Cauldron.</p>
-      </div>
-      <style jsx>{
-        @media (min-width: 1280px) {
-          .wrap {
-            padding-left: 0px;
-            padding-right: 0px;
-          }
-        }
-        @media (min-width: 1024px) {
-          .wrap {
-            margin-left: auto;
-            margin-right: auto;
-          }
-        }
-        @media (min-width: 768px) {
-          .wrap {
-            padding-left: 2.5rem;
-            padding-right: 2.5rem;
-          }
-        }
-        .logo{
-          width:70px;
-        }
-        .wrap {
-          max-width: 80rem;
-          padding: 0.5rem;
-          width: 100%;
-        }
-        .header {
-          font-size: 2rem;
-          font-weight: 500;
-          color: #0f1014;
-        }
-        .content {
-          margin-top: 1rem;
-        }
-        .paragraph {
-          color: #313131;
-        }
-        .link {
-          color: #212121;
-          text-decoration: underline;
-        }
-        .team {
-          font-weight: 500;
-          color: #212121;
-        }
-        .footer {
-          margin-top: 1rem;
-        }
-        .footer-text {
-          font-size: 0.875rem;
-          color: #515151;
-        }
-      }</style>
-    </div>`;
+  return ` <div
+    style="
+      font-family: Arial, sans-serif;
+      max-width: 600px;
+      margin: 0 auto;
+      padding: 20px;
+      border: 1px solid #e0e0e0;
+    "
+  >
+    <div style="text-align: center">
+      <!-- <img  alt="Cauldron" style="width: 70px;"/> -->
+      <a href="https://cauldron.live/"><img src="https://i.ibb.co/2P7M74b/Frame-65.png" style="width: 70px;" alt="Frame-65" ></a>
+    </div>
+    <h1 style="text-align: left; color: #0f1014">Welcome to Cauldron!</h1>
+    <div style="color: #313131; line-height: 1.5">
+      <p>Hello ${name},</p>
+      <p>
+        Thank you for your interest in being one of Cauldron's first users. We
+        truly appreciate your support!
+      </p>
+      <p>
+        We've been working diligently on
+        <a
+          href="https://cauldron.live"
+          style="color: #212121; text-decoration: underline"
+        >
+          cauldron.live </a
+        >, since May 2024, and we're excited to announce that it will be
+        released in August. Stay tuned for an incredible experience!
+      </p>
+      <p>
+        To get the latest updates, follow us on Twitter:
+        <a
+          href="https://x.com/cauldron_live"
+          style="color: #212121; text-decoration: underline"
+        >
+          @cauldron_live
+        </a>
+      </p>
+      <p>
+        Right now it's just 3 people working on this idea we believe is quite
+        amazing. We have colleges, examinations, internships, and other
+        projects, but we will ship this very soon.
+      </p>
+      <p>Stay tuned and thank you for joining the queue.</p>
+      <p>Best regards,</p>
+      <p style="font-weight: 500; color: #212121">The Cauldron Team</p>
+    </div>
+    <div
+      style="
+        border-top: 1px solid #e0e0e0;
+        margin-top: 20px;
+        padding-top: 10px;
+        text-align: center;
+      "
+    >
+      <p style="font-size: 0.875rem; color: #515151">&copy; 2024 Cauldron.</p>
+    </div>
+  </div>
+`;
 };
 
 export default Mail;
