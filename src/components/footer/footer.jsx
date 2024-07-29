f4b4e0ad6dd70580ac6ca1c0495cac806991dddb
Lokendra Kushwah <118094744+Lokendrakushwah12@users.noreply.github.com>
2024-07-29 23:41:23 +0530

    smooth scroll

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -9,7 +9,7 @@ export default function Footer() {
     <>
       <div className="relative overflow-hidden bg-[rgb(11,11,14)] pt-10 text-white">
         <div className="absolute top-0 z-0 h-[1px] w-full bg-gradient-to-r from-[#0b0b0e] via-[#FEFEFE] to-[#0b0b0e]"></div>
-        <div className="absolute -bottom-[100%] md:-bottom-[150%] left-1/2 z-0 h-[375px] w-[375px] -translate-x-1/2 -translate-y-1/2 rounded-full bg-[#0071E3] blur-[200px]"></div>
+        <div className="absolute -bottom-[100%] left-1/2 z-0 h-[375px] w-[375px] -translate-x-1/2 -translate-y-1/2 rounded-full bg-[#0071E3] blur-[200px] md:-bottom-[150%]"></div>
         <div className="mb-20 flex flex-wrap-reverse justify-between">
           <div className="m-auto flex flex-col justify-center gap-2 pl-2 pt-20 md:pl-10 md:pt-0 lg:ml-32 lg:justify-normal lg:pl-0">
             <img
@@ -26,18 +26,18 @@ export default function Footer() {
               © 2024 Cauldron All rights reserved.
             </p>
             <div className="m-auto flex items-center gap-5 md:m-0">
-              <Link to="https://discord.gg/VDrzngkvZ2">
+              <Link to="https://discord.gg/VDrzngkvZ2" target="_blank">
                 <img
                   src={Discord}
                   alt="Discord"
                   className="w-8 opacity-80 transition-all hover:opacity-100"
                 />
               </Link>
-              <Link to="https://x.com/Cauldron_live">
+              <Link to="https://x.com/Cauldron_live" target="_blank">
                 <img
                   src={X}
                   alt="X"
-                  className="w-8 opacity-80 transition-all hover:opacity-100"
+                  className="w-7 opacity-80 transition-all hover:opacity-100"
                 />
               </Link>
             </div>
@@ -58,6 +58,7 @@ export default function Footer() {
           <motion.div
             whileHover={{
               color: "#0071E3",
+              transition: { duration: 0.1 },
             }}
           >
             <Link to="/termsofservice">Terms of Service</Link>
@@ -65,6 +66,7 @@ export default function Footer() {
           <motion.div
             whileHover={{
               color: "#0071E3",
+              transition: { duration: 0.1 },
             }}
           >
             <Link to="/privacypolicy">Privacy Policy</Link>
