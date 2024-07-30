f59c982416049375f0dc0c76773fe6a116852f7a
Lokendra Kushwah <118094744+Lokendrakushwah12@users.noreply.github.com>
2024-07-30 17:21:53 +0530

    smooth scroll ease

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,10 @@ import { useEffect } from "react";
 
 function App() {
   useEffect(() => {
-    const lenis = new Lenis();
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+    });
 
     function raf(time) {
       lenis.raf(time);
